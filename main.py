955aacbb069c4f15374f29c206acddd467ab7383
rileytrent <rileytrent8@gmail.com>
2024-04-10 18:57:15 -0400

    Added a word counter to the bookbot function

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    words = file_contents.split()
+    counter = 0 
+    for i in words:
+        counter += 1 
+    print(f"{counter} words in file")
 
 if __name__ == '__main__':
     main()
