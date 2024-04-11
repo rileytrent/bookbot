5ba8ec458abfbffed3fab9ce56893135c05b6807
rileytrent <rileytrent8@gmail.com>
2024-04-10 21:01:30 -0400

    update code with comments on what the code does

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def main():
+    # enter a path to a file in the text variable
     text = "books/frankenstein.txt"
     with open(text) as f:
         file_contents = f.read()
-    #print(file_contents)
+    # gets the contents of the text file
     words = file_contents.split()
+    # counts the words in the fild
     word_counter = (len(words))
+    # formatting for the end report 
     print("")
     print(f"--- Report of {text} file ---")
     print("")
@@ -12,13 +15,14 @@ def main():
     print("")
     print(f"This file has {word_counter} words in it.")
     print("")
+    #creates a Dictionary of all the letters in the english language as the key and the frequency of each letter in the text file as the value
     english_letters = 'abcdefghijklmnopqrstuvwxyz'
     lower_words = file_contents.lower()
     letter_count_dict = {letter: 0 for letter in english_letters}
     for char in lower_words:
         if char in english_letters:
             letter_count_dict[char] += 1
-    #print(letter_count_dict)
+    # prints every key and value in the format below 
     for key, value in letter_count_dict.items():
         print(f"The '{key}' character was found {value} times")
     print("")
