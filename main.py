42a12112f2577cd07a2e4b4b91a81f47e0cbe424
rileytrent <rileytrent8@gmail.com>
2024-04-10 20:41:10 -0400

    Bookbot now reads a specific file and produces a report of how many words are in the file and the frequency that each letter of the english language appears in the file.

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    text = "books/frankenstein.txt"
+    with open(text) as f:
         file_contents = f.read()
-    print(file_contents)
+    #print(file_contents)
     words = file_contents.split()
-    counter = 0 
-    for i in words:
-        counter += 1 
-    print(f"{counter} words in file")
+    word_counter = (len(words))
+    print("")
+    print(f"--- Report of {text} file ---")
+    print("")
+    print("---------------------------------------------")
+    print("")
+    print(f"This file has {word_counter} words in it.")
+    print("")
+    english_letters = 'abcdefghijklmnopqrstuvwxyz'
+    lower_words = file_contents.lower()
+    letter_count_dict = {letter: 0 for letter in english_letters}
+    for char in lower_words:
+        if char in english_letters:
+            letter_count_dict[char] += 1
+    #print(letter_count_dict)
+    for key, value in letter_count_dict.items():
+        print(f"The '{key}' character was found {value} times")
+    print("")
+    print("--------------- End of Report ---------------")
+    print("")
 
 if __name__ == '__main__':
     main()
